bab6b0d09d24a746b83fcaa7187bd1b88bde7b31
michelp76 <michel.puissant@inextenso.fr>
2024-02-08 09:29:12 +0100

    Rétro pédale Typescript ^3.2.1 sinon rien ne compile

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,16 +2,16 @@ import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    return {
-      headers: {
-        authorization: 'Bearer ' + token,
-      },
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
     }
   }
-})
+});
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000',
